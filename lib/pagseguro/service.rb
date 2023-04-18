b0fe6502973e73a3981dcf00809adddb5ba5020e
Alex Coelho <alexcvcoelho@gmail.com>
2023-04-18 06:50:39 -0300

    feat(pagseguro): change  pagseguro sdk

--- lib/pagseguro/service.rb
+++ lib/pagseguro/service.rb
@@ -7,29 +7,28 @@ module PagSeguro; end
 
 ## create remote objects on PagSeguro
 class PagSeguro::Service < Payment::Service
-    include PagSeguro
 
     def create_payment(amount, order_id, sender, items)
-        payment = PagSeguro::PaymentRequest.new
-        payment.credentials = PagSeguro::AccountCredentials.new(Setting.get('pagseguro_email'), Setting.get('pagseguro_token'))
-        payment.reference = order_id
-        payment.notification_url = Rails.application.secrets.notification_url
-        payment.redirect_url = Rails.application.secrets.redirect_url
-        payment.max_uses = 1
-        payment.max_age = 30000  # em segundos
-        payment.sender = sender
-        items.each do |item|
-            payment.items << item
-        end
+        # payment = PagSeguro::PaymentRequest.new
+        # payment.credentials = PagSeguro::AccountCredentials.new(Setting.get('pagseguro_email'), Setting.get('pagseguro_token'))
+        # payment.reference = order_id
+        # payment.notification_url = Rails.application.secrets.notification_url
+        # payment.redirect_url = Rails.application.secrets.redirect_url
+        # payment.max_uses = 1
+        # payment.max_age = 30000  # em segundos
+        # payment.sender = sender
+        # items.each do |item|
+        #     payment.items << item
+        # end
 
-        puts "=> REQUEST"
-        puts PagSeguro::PaymentRequest::RequestSerializer.new(payment).to_params
-        response = payment.register
-        if not response.errors.any?
-            return {
-                code: response.code,
-                url: response.url
-            }
-        end
+        # puts "=> REQUEST"
+        # puts PagSeguro::PaymentRequest::RequestSerializer.new(payment).to_params
+        # response = payment.register
+        # if not response.errors.any?
+        #     return {
+        #         code: response.code,
+        #         url: response.url
+        #     }
+        # end
     end
 end
